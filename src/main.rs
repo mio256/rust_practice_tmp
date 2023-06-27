559fbc7e80f7a6232479995253e8287d870c8f9e
mio256 <reiya@woorth.io>
2023-06-27 10:30:58 +0900

    :fire:

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,64 @@
 #[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
+struct Rational {
+    num: isize,
+    den: isize,
 }
 
-impl Rectangle {
-    fn area(&self) -> u32 {
-        self.width * self.height
+impl Rational {
+    fn new(n: isize, d: isize) -> Self {
+        Self { num: n, den: d }
     }
-    fn area_double(&self) -> u32 {
-        self.width * self.height * 2
+    fn float(&self) -> f32 {
+        self.num as f32 / self.den as f32
     }
-    fn zero(&mut self) {
-        self.width = 0;
-        self.height = 0;
+    fn gcd(a: isize, b: isize) -> isize {
+        if b == 0 {
+            return a;
+        }
+        return Self::gcd(b, a % b);
+    }
+    fn reduse(&mut self) {
+        let n = Self::gcd(self.num, self.den);
+        self.num /= n;
+        self.den /= n;
+        if self.den < 0 {
+            self.num *= -1;
+            self.den *= -1;
+        }
+    }
+    fn add(r1: &Self, r2: &Self) -> Self {
+        Self {
+            num: r1.num * r2.den + r2.num * r1.den,
+            den: r1.den * r2.den,
+        }
+    }
+    fn sub(r1: &Self, r2: &Self) -> Self {
+        Self {
+            num: r1.num * r2.den - r2.num * r1.den,
+            den: r1.den * r2.den,
+        }
+    }
+    fn mul(r1: &Self, r2: &Self) -> Self {
+        Self {
+            num: r1.num * r2.num,
+            den: r1.den * r2.den,
+        }
+    }
+    fn div(r1: &Self, r2: &Self) -> Self {
+        Self {
+            num: r1.num * r2.den,
+            den: r1.den * r2.num,
+        }
     }
 }
 
 fn main() {
-    let mut rect1 = Rectangle {
-        width: 30,
-        height: 50,
-    };
-
-    dbg!(rect1.area());
-    dbg!(rect1.area_double());
-    dbg!(rect1.zero());
-    dbg!(rect1.area());
-    dbg!(rect1.area_double());
+    let r1 = Rational::new(1, 3);
+    let r2 = Rational::new(2, 3);
+    dbg!(&r1, &r2);
+    let mut r = Rational::add(&r1, &r2);
+    dbg!(&r);
+    r.reduse();
+    dbg!(&r);
+    dbg!(r.float());
 }
