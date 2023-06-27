0b8232e265888bffbe7c153d1462a73a2b61b5cc
mio256 <reiya@woorth.io>
2023-06-27 09:39:24 +0900

    :fire:

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,21 @@ impl Rectangle {
     fn area_double(&self) -> u32 {
         self.width * self.height * 2
     }
+    fn zero(&mut self) {
+        self.width = 0;
+        self.height = 0;
+    }
 }
 
 fn main() {
-    let rect1 = Rectangle {
+    let mut rect1 = Rectangle {
         width: 30,
         height: 50,
     };
 
+    dbg!(rect1.area());
+    dbg!(rect1.area_double());
+    dbg!(rect1.zero());
     dbg!(rect1.area());
     dbg!(rect1.area_double());
 }
